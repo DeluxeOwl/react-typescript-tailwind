e67f677e9150a28f7efc51b49572666a0f4eb615
DeluxeOwl <54327698+DeluxeOwl@users.noreply.github.com>
2023-01-27 13:14:09 +0200

    feat: improved visual ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,36 @@
 import ky from "ky";
+import { useState } from "react";
+
+interface People {
+  id: string;
+  name: string;
+  age: number;
+}
 
 function App() {
+  const [people, setPeople] = useState<People[]>([]);
+
+  const handleGetPeople = async () => {
+    const json = await ky.get("/people").json();
+    setPeople(json as People[]);
+  };
+
   return (
-    <div>
-      Hello, world!
-      <div>
-        <button
-          onClick={async () => {
-            const json = await ky.get("/people").json();
-            console.log(json);
-          }}
-          className="active:translate-y-[1px] transition-all shadow-md ml-2 w-fit px-4 py-2 h-fit bg-black text-white rounded-md"
-        >
-          Get some people!!
-        </button>
-      </div>
+    <div className="bg-zinc-900 min-h-screen w-full flex flex-col space-y-2 justify-center items-center text-white">
+      <button
+        onClick={handleGetPeople}
+        className="active:translate-y-1 transition-all inline-flex justify-center rounded-lg text-sm font-semibold py-3 px-4 bg-neutral-800 text-white hover:bg-neutral-700 shadow-md"
+      >
+        Get some people!!
+      </button>
+      <ul className="list-disc space-y-4 ">
+        {people.map((p) => (
+          <li
+            key={p.id}
+            className="bg-zinc-700 p-1 rounded-md"
+          >{`${p.name}, ${p.age} years old`}</li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,14 +1,15 @@
 // src/mocks/handlers.js
 import { rest } from "msw";
-import { faker } from '@faker-js/faker';
+import { faker } from "@faker-js/faker";
 
-const people = Array.from({length:8}).map(_=>({name:faker.name.fullName(), something:faker.random.numeric(2)}));
+const people = Array.from({ length: 8 }).map((_) => ({
+  id: faker.datatype.uuid(),
+  name: faker.name.fullName(),
+  age: faker.datatype.number({ min: 20, max: 30 }),
+}));
 
 export const handlers = [
   rest.get("/people", (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json(people)
-    );
+    return res(ctx.status(200), ctx.json(people));
   }),
 ];
